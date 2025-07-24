03403bce071a1dd22e369de38884c6a0863bc465
EsterTavares2024 <estergestaodeprojetos@gmail.com>
2025-07-24 00:10:22 -0300

    ajustes 6

--- config/db.php
+++ config/db.php
@@ -1,12 +1,10 @@
 <?php
 
-// Detecta se está rodando no GitHub Actions
 $isCI = getenv('GITHUB_ACTIONS') === 'true';
 
-// Define host do banco de dados dependendo do ambiente
-$host = $isCI ? '127.0.0.1' : 'db';  // 👈 importante
-$user = 'admin';
-$pass = 'admin123';
+$host = '127.0.0.1';
+$user = $isCI ? 'root' : 'admin';
+$pass = $isCI ? 'root' : 'admin123';
 $dbname = 'db';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
